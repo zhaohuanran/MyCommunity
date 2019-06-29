f0b141fe56078a837313b17f692a827e1ada87dc
Nauh <zhaoguixinc@foxmail.com>
2019-06-30 00:45:18 +0800

    init repo

---
+++ src/test/java/learn/mycommunity/MycommunityApplicationTests.java
@@ -0,0 +1,16 @@
+package learn.mycommunity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MycommunityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
